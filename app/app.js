2c4bbf03e11749050387ea754375d34bb6401622
Paola Ortiz de Zevallos <paolaozv@gmail.com>
2016-08-25 20:29:14 -0500

    modificacion

--- app/app.js
+++ app/app.js
@@ -8,10 +8,10 @@ else {
    	if (numero % 2 != 0)
    		document.write("Odd!");
    	else {
-   		if (numero > 2 && numero < 5) 
+   		if (numero >= 2 && numero < 5) 
    			document.write("Good!");
    		else {
-   			if (numero > 6 && numero <= 20) 
+   			if (numero >= 6 && numero <= 20) 
    				document.write("Great!");		
    			else {
    				if (numero > 20)
